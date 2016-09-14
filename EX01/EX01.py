6396cdaf2688b839ead649d473b869a13f6c289d
Egils Looga <Egils Looga>
2016-09-14 09:44:03 +0300

    EX01 2.3

--- EX01/EX01.py
+++ EX01/EX01.py
@@ -8,6 +8,8 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
         a = -1
     elif jump_distance2 / sleep2 > jump_distance1 / sleep1 and pos2 > pos1:
         a = -1
+    elif jump_distance1 / sleep1 == jump_distance2 / sleep2:
+        a = -1
     else:
         if pos1 > pos2:
             while pos1 != pos2:
@@ -25,8 +27,3 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
                     a = -1
     return a
 
-print(meet_me(1, 2, 1, 2, 1, 1))
-print(meet_me(1, 2, 3, 4, 5, 5))
-print(meet_me(10, 7, 7, 5, 8, 6))
-print(meet_me(100, 7, 4, 300, 8, 6))
-print(meet_me(1, 7, 1, 15, 5, 1))
