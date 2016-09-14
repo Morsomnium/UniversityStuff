2bce2309b25cb91b6bb616641bb067b373eac738
Egils Looga <Egils Looga>
2016-09-14 09:22:01 +0300

    EX01 2.1

--- EX01/EX01.py
+++ EX01/EX01.py
@@ -1,13 +1,15 @@
 from fractions import Fraction
+
+
 def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
-    if pos1 == pos2 :
+    if pos1 == pos2:
         a = pos1
-    elif jump_distance1/sleep1 > jump_distance2/sleep2 and pos1 > pos2 :
+    elif jump_distance1 / sleep1 > jump_distance2 / sleep2 and pos1 > pos2:
         a = -1
-    elif jump_distance2/sleep2 > jump_distance1/sleep1 and pos2 > pos1 :
+    elif jump_distance2 / sleep2 > jump_distance1 / sleep1 and pos2 > pos1:
         a = -1
     else:
-        if pos1>pos2:
+        if pos1 > pos2:
             while pos1 != pos2 and pos1 > pos2:
                 pos1 += Fraction(jump_distance1, sleep1)
                 a = pos1
@@ -22,4 +24,3 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
             else:
                 a = -1
     return a
-
