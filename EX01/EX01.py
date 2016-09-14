c5f6b62a1d7b50b6252eb9cb9baa2ba9249006db
Egils Looga <Egils Looga>
2016-09-14 09:32:38 +0300

    EX01 2.2

--- EX01/EX01.py
+++ EX01/EX01.py
@@ -10,17 +10,23 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
         a = -1
     else:
         if pos1 > pos2:
-            while pos1 != pos2 and pos1 > pos2:
+            while pos1 != pos2:
                 pos1 += Fraction(jump_distance1, sleep1)
                 a = pos1
                 pos2 += Fraction(jump_distance2, sleep2)
-            else:
-                a = -1
+                if pos1 > pos2:
+                    a = -1
         elif pos2 > pos1:
-            while pos1 != pos2 and pos2 > pos1:
+            while pos1 != pos2:
                 pos1 += Fraction(jump_distance1, sleep1)
                 a = pos1
                 pos2 += Fraction(jump_distance2, sleep2)
-            else:
-                a = -1
+                if pos2 > pos1:
+                    a = -1
     return a
+
+print(meet_me(1, 2, 1, 2, 1, 1))
+print(meet_me(1, 2, 3, 4, 5, 5))
+print(meet_me(10, 7, 7, 5, 8, 6))
+print(meet_me(100, 7, 4, 300, 8, 6))
+print(meet_me(1, 7, 1, 15, 5, 1))
