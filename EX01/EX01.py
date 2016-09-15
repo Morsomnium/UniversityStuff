e51839de93376311925d1e28272c45b57e39a27d
Egils Looga <edloog@ttu.ee>
2016-09-15 13:00:04 +0300

    EX01 4.0

--- EX01/EX01.py
+++ EX01/EX01.py
@@ -1,7 +1,25 @@
+"""
+Tracks two bunnies as they jump
+
+:Author: Egils Looga
+:version: 4.0
+:failed: test_cant_catch_me
+"""
 from fractions import Fraction
 
 
 def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
+    """
+    Does all what you need
+
+    :param pos1: Start position of first bunny
+    :param jump_distance1: How long is no1s jump
+    :param sleep1: no1 jump cooldown
+    :param pos2: Start position of second bunny
+    :param jump_distance2: How long is no2s jump
+    :param sleep2: no2 jump cooldown
+    :return: its a but technically its pos1 or pos2
+    """
     if pos1 == pos2:
         a = pos1
     elif (jump_distance1 / sleep1 > jump_distance2 / sleep2 and pos1 > pos2) or (jump_distance2 / sleep2 > jump_distance1 / sleep1 and pos2 > pos1):
