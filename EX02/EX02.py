1d8cb676e1e5286ea4d7de9f1c1af7217518d682
Egils Looga <edloog@ttu.ee>
2016-09-20 20:29:54 +0300

    EX02 0.5

--- EX02/EX02.py
+++ EX02/EX02.py
@@ -2,7 +2,7 @@
 Normalize and solve equations.
 
 :Author: Egils Looga
-:version: 0.3
+:version: 0.4
 :failed: all ;)
 """
 
@@ -11,18 +11,20 @@ import re
 
 
 def find_square(equation):
-    p = re.compile('([+-=])*\s*[1-9]?\d*(x2)')
+    global square
+    global square_pos
+    p = re.compile('(\+|-|=)?\s*[1-9]?\d*(x2)')
     square = p.search(equation)
     if square is not None:
         square_pos = square.start(0)
         square = square.group().strip()
+        if square[-1] == '+' or square[-1] == '-' or square[-1] == '=':
+            square = square[:-1].strip()
         if square[0] == '=':
-            square = square[1:]
+            square = square[1:].strip()
+        square = one_checker(space_insert(square).strip())
         if square_pos > eq_pos:
-            if square[0] != '-':
-                square = '-' + square[1:]
-            else:
-                square = '+' + square[1:]
+            square = side_swap(square)
     else:
         square = ''
         square_pos = 0
@@ -30,53 +32,59 @@ def find_square(equation):
 
 
 def find_linear(equation):
-    p = re.compile('[+-=]\s*\d*x[^2]')
+    global linear
+    global linear_pos
+    p = re.compile('(\+|-|=)?\s*\d*x[^2]')
     linear = p.search(equation)
     if linear is not None:
         linear_pos = linear.start(0)
         linear = linear.group().strip()
         if linear[-1] == '+' or linear[-1] == '-' or linear[-1] == '=':
-            linear = linear[:-1]
+            linear = linear[:-1].strip()
         if linear[0] == '=':
-            linear = linear[1:]
+            linear = linear[1:].strip()
+        linear = one_checker(space_insert(linear).strip())
         if linear_pos > eq_pos:
-            if linear[0] != '-':
-                linear = '-' + linear[1:]
-            else:
-                linear = '+' + linear[1:]
+            linear = side_swap(linear)
     else:
         linear = ''
         linear_pos = 0
-    return linear, linear_pos
 
 
 def find_free(equation):
-    p = re.compile('(\+|-)(\W|\s)?[1-9](\d+)?(\W|$|\s)')
+    global free
+    global free_pos
+    p = re.compile('(\+|-)?(\W|\s)?[^x][1-9](\d+)?[^x]')
     free = p.search(equation)
     if free is not None:
         free_pos = free.start()
         free = free.group().strip()
+        print(free, '++22++')
         if free[-1] == '+' or free[-1] == '-' or free[-1] == '=':
-            free = free[:-1]
+            free = free[:-1].strip()
         if free[0] == '=':
-            free = free[1:]
+            free = free[1:].strip()
+        free = space_insert(free)
+        if free_pos > eq_pos:
+            free = side_swap(free)
+        free = free.strip()
     else:
         free = ''
         free_pos = 0
-    return free, free_pos
 
 
 def space_insert(checked):
-    p = re.compile('(\+|-)\s')
+    p = re.compile('(\+|-)')
     var1 = p.search(checked)
-    if var1 is None and len(checked) > 0 and(checked[0] == '+' or checked[0] == '-'):
+    print(checked)
+    if var1 is not None and checked[1] != ' ':
         checked = checked[0] + ' ' + checked[1:]
     return checked
 
 
 def one_checker(checked):
-    d = re.compile('1(x2|x)')
-    if d.search(checked) is not None and(d.search(checked).group()[:-1] == '1' or d.search(checked).group()[:-2] == '1'):
+    d = re.compile('(\s|^)1x2?')
+    if d.search(checked) is not None:
         if checked[0] == '-' or checked[0] == '+':
             checked = checked[:2] + checked[3:]
         else:
@@ -84,23 +92,49 @@ def one_checker(checked):
     return checked
 
 
+def side_swap(var1):
+    if var1[0] != '-':
+        if var1[0] == '+':
+            var1 = '-' + var1[1:]
+        else:
+            var1 = '- ' + var1
+    else:
+        var1 = '+' + var1[1:]
+    return var1
+
+
+def xminus():
+    global square
+    global linear
+    global free
+    print(square[0])
+    if square[0] == '-':
+        square = side_swap(square)
+        if linear != '':
+            linear = side_swap(linear)
+        if free != '':
+            free = side_swap(free)
+    return square, linear, free
+
+
 def normalize_equation(equation):
     global eq_pos
     eq_pos = re.search('=', equation).start()
     equation = ' ' + equation + ' '
-    square = one_checker(space_insert(find_square(equation)[0]))
-    linear = one_checker(space_insert(find_linear(equation)[0]))
-    free = space_insert(find_free(equation)[0])
+    find_square(equation)
+    find_linear(equation)
+    find_free(equation)
+    xminus()
     equation = '{} {} {} = 0'.format(square, linear, free).strip()
+    if equation[0] == '+':
+        equation = equation[2:]
     print('square:', square)
     print('linear:', linear, len(linear))
     print('free:', free)
     return equation
 
-print(normalize_equation("- 3 = - 4864648464x2 + 15151x"))
-# print(normalize_equation("2x2 + 2x = 3"))
 # print(normalize_equation("x2 + 2x = 3"))  # "x2 + 2x - 3 = 0"
-# print(normalize_equation("0 = 3 + 1x2"))  # "x2 + 3 = 0"
+print(normalize_equation("0 = 3 + 1x2"))  # "x2 + 3 = 0"
 # print(normalize_equation("2x + 2 = 2x2"))  # "2x2 - 2x - 2 = 0"
 
 
