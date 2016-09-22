06c68589a5a2e5888ea30e1d4a13768234144ff9
Egils Looga <edloog@ttu.ee>
2016-09-22 13:05:18 +0300

    EX01 4.0.2

--- EX01/EX01.py
+++ EX01/EX01.py
@@ -21,7 +21,7 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
     :return: its a but technically its pos1 or pos2
     """
     if pos1 == pos2:
-        return pos1
+        pos1 = pos2
     elif (jump_distance1 / sleep1 > jump_distance2 / sleep2 and pos1 > pos2) or (jump_distance2 / sleep2 > jump_distance1 / sleep1 and pos2 > pos1):
         pos1 = -1
     elif jump_distance1 / sleep1 == jump_distance2 / sleep2:
