d59859a06d8ea8175ac240959337950f637e7eba
Egils Looga <edloog@ttu.ee>
2016-09-22 12:57:12 +0300

    EX01 4.3

--- EX01/EX01.py
+++ EX01/EX01.py
@@ -2,7 +2,7 @@
 Tracks two bunnies as they jump.
 
 :Author: Egils Looga.
-:version: 4.1.
+:version: 4.3.
 :failed: test_cant_catch_me.
 """
 from fractions import Fraction
@@ -21,7 +21,7 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
     :return: its a but technically its pos1 or pos2
     """
     if pos1 == pos2:
-        a = pos1
+        return pos1
     elif (jump_distance1 / sleep1 > jump_distance2 / sleep2 and pos1 > pos2) or (jump_distance2 / sleep2 > jump_distance1 / sleep1 and pos2 > pos1):
         pos1 = -1
     elif jump_distance1 / sleep1 == jump_distance2 / sleep2:
@@ -34,8 +34,6 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
                 if pos2 > pos1:
                     pos1 = -1
                     break
-            else:
-                pos1 = -1
         elif pos2 > pos1:
             while pos1 != pos2:
                 pos1 += Fraction(jump_distance1, sleep1)
@@ -43,8 +41,6 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
                 if pos1 > pos2:
                     pos1 = -1
                     break
-            else:
-                pos1 = -1
     return pos1
 
 print(meet_me(0, 15, 6, 5, 0, 3))
