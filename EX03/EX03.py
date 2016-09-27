4eb1943fdb7c948c91b5a076afb2458c7202fe7c
Egils Looga <edloog@ttu.ee>
2016-09-27 21:43:10 +0300

    EX03 1.8.3

--- EX03/EX03.py
+++ EX03/EX03.py
@@ -2,7 +2,7 @@
 Decode coded lists.
 
 :Author: Egils Looga
-:version: 1.8.1
+:version: 1.8.3 final
 :failed: not_square
 """
 
@@ -41,7 +41,6 @@ def transpose(text):
     or ["AA", "B", "C"] => ["ABC", "A"]
     or ["A", "B", "CC"] => ["ABC", "  C"]
     """
-    # YOUR SOLUTION HERE
     wid = int(len(max(text, key=len)))
     for i in range(len(text)):
         if len(text[i]) < wid:
@@ -54,7 +53,6 @@ def transpose(text):
             tab += list1[y][x]
         list2.append(tab)
         tab = ''
-    print('list2:', list2)
     for i in range(len(list2)):
         while list2[i][-1] == ' ':
             list2[i] = list2[i][:-1]
@@ -92,16 +90,6 @@ def find_matching(original, transposed):
             original[i] = original[i].ljust(wid)
     original = magic_touch(original)
     transposed = magic_touch(transposed)
-    """
-    for i in range(len(original)):
-        original[i] = original[i].split()
-        original2.extend(original[i])
-    for i in range(len(transposed)):
-        transposed[i] = transposed[i].split()
-        transposed2.extend(transposed[i])"""
-    # print('orig2:', original2)
-    # print('orig:', original)
-    # print('trans2:', transposed2)
     match = list(set(transposed).intersection(original))
     for i in range(len(original) - 1, 0, -1):
         if original[i] not in match:
@@ -113,9 +101,3 @@ def find_matching(original, transposed):
         if i % 2 == 1:
             result.append(original[i - 1] + ' ' + original[i])
     return result
-
-# print('read file:', read_file('secretagents.txt'))
-# print(transpose(read_file('secretagents.txt')))
-
-# original = read_file("secretagents.txt")
-# print(find_matching(original, transpose(original)))
