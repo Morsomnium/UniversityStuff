5a9b9d9167fd974b6c5640777d3d90166aef3842
Egils Looga <edloog@ttu.ee>
2016-10-06 12:11:00 +0300

    EX04 reset

--- EX04/EX04.py
+++ EX04/EX04.py
@@ -21,25 +21,6 @@ def read_words(filename):
         return collections.Counter(f.read().split())
 
 
-def funk(sentence, word_dict, pass_list, guessed_letters):
-    for words in sentence:
-        for word in word_dict:
-            for m in re.finditer("[a-z]", words):
-                if len(words) == len(word):
-                    if m.group() in word:
-                        if word[m.start()] == m.group():
-                            pass_list[word] = word_dict.get(word)
-                    else:
-                        try:
-                            del pass_list[word]
-                        except KeyError:
-                            pass
-            if len(words) == len(word):
-                if len(set(guessed_letters).intersection(word)) == 0:
-                    pass_list[word] = word_dict.get(word)
-    return pass_list
-
-
 def guess(sentence, guessed_letters, word_dict):
     """
     Offer the letter which would most probably give the best result.
@@ -55,7 +36,21 @@ def guess(sentence, guessed_letters, word_dict):
     letter_list = collections.Counter()
     sentence = sentence.split()
 
-    pass_list = funk(sentence, word_dict, pass_list, guessed_letters)
+    for words in sentence:
+        for word in word_dict:
+            for m in re.finditer("[a-z]", words):
+                if len(words) == len(word):
+                    if m.group() in word:
+                        if word[m.start()] == m.group():
+                            pass_list[word] = word_dict.get(word)
+                    else:
+                        try:
+                            del pass_list[word]
+                        except KeyError:
+                            pass
+            if len(words) == len(word):
+                if len(set(guessed_letters).intersection(word)) == 0:
+                    pass_list[word] = word_dict.get(word)
     for word in pass_list:
         for letter in word:
             letter_list[letter] += 1 * word_dict.get(word)
