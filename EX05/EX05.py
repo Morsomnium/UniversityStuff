1e7817476e24a2af245632476ebfff9f467cb6be
Egils Looga <edloog@ttu.ee>
2016-10-09 16:47:22 +0300

    EX05 4.0

--- EX05/EX05.py
+++ EX05/EX05.py
@@ -35,21 +35,22 @@ def test_one_mult_x2():
 def test_one_mult_x():
     """Clear 1 multiplier."""
     assert EX02.normalize_equation("1x + 3") == "x + 3 = 0"
+
+
 def test_one_mult_f():
-    """Clear 1 multiplier"""
+    """Clear 1 multiplier."""
     assert EX02.normalize_equation("13") == "13 = 0"
 
 
-"""def test_zero_mult():
-    """"""Clear 0 multiplier."""""""
+def test_zero_mult():
+    """Clear 0 multiplier."""
     assert EX02.normalize_equation("0x2 + 5x + 3") == "5x + 3 = 0"
     assert EX02.normalize_equation("x2 + 0x + 3") == "x2 + 3 = 0"
     assert EX02.normalize_equation("x2 + 5x + 0") == "x2 + 5x= 0"
 
 
 def test_sign_del():
-    """"""Remove + sign in the beginning.""""""
+    """Remove + sign in the beginning."""
     assert EX02.normalize_equation("+x2 + 5x + 3") == "x2 + 5x + 3 = 0"
     assert EX02.normalize_equation("+ 5x + 3") == "5x + 3 = 0"
     assert EX02.normalize_equation("+ 3") == "+ 3 = 0"
-"""
