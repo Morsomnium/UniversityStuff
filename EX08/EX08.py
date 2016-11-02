1a3b2825752d6b62e5ce403ff13e3f6e8d83acc6
Egils Looga <edloog@ttu.ee>
2016-11-02 19:53:12 +0200

    EX08 1.0

--- EX08/EX08.py
+++ EX08/EX08.py
@@ -1,7 +1,8 @@
-regex_01 = "r'\w(at)\w*'"
-regex_02 = "r''"
-regex_03 = "r'\s*'"
-regex_04 = "r''"
-regex_05 = "r'(1001)+'"
-regex_06 = "r''"
-regex_07 = "r''"
+"""Regex-play."""
+regex_01 = "r'^\w(at)\w*$'"  #pass
+regex_02 = "r'jbnffkjbn'"
+regex_03 = "r'\s*'"  # pass
+regex_04 = "r'^0x[a-f0-9]+'"  # pass
+regex_05 = "r'(1001)+'"  # pass
+regex_06 = "r'^[^0]*(1[^1]*1[^1]*)*(0[^0]*0[^0]*)*$'"
+regex_07 = "r'dgbhkfxj'"
