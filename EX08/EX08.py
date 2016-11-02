1cab61ff9d551fb7856d308599eb774e170d21f4
Egils Looga <edloog@ttu.ee>
2016-11-02 20:14:06 +0200

    EX08 1.0

--- EX08/EX08.py
+++ EX08/EX08.py
@@ -2,7 +2,7 @@
 regex_01 = "r'^\w(at)\w*$'"  #pass
 regex_02 = "r'jbnffkjbn'"
 regex_03 = "r'\s*'"  # pass
-regex_04 = "r'^0x[a-f0-9]+'"  # pass
+regex_04 = re.compile(r'^0x[a-f0-9]+')  # pass
 regex_05 = "r'(1001)+'"  # pass
-regex_06 = "r'^[^0]*(1[^1]*1[^1]*)*(0[^0]*0[^0]*)*$'"
+regex_06 = "r'^(0[^0]*0[^0]*)*$'"
 regex_07 = "r'dgbhkfxj'"
