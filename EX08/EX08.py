cc25a6d3cedb4e40bb844762807e438b99052011
Egils Looga <edloog@ttu.ee>
2016-11-03 13:31:13 +0200

    EX08 1.8

--- EX08/EX08.py
+++ EX08/EX08.py
@@ -1,8 +1,8 @@
 """Regex-play."""
-regex_01 = '^\w(at)\w?$'  # pass+
+regex_01 = '^\w(at)\w?$'  # pass
 regex_02 = '(\w(a\wa)+)|((a\wa)+\w)'
-regex_03 = r'^((\w|\s)*(\s{2}\S*)+(\w|\s)*)|()$'  # -
+regex_03 = '^((\w|\s)*(\S*\s{2}\S*)+(\w|\s)*)$'  # -
 regex_04 = '^0x[a-f0-9]+$'  # pass
 regex_05 = '^(1|0)*(1001)+(1|0)*$'  # pass
-regex_06 = '^(0[^0]*0[^0]*)*$'
+regex_06 = '^(0[^0]*0[^0]*)*$'  #
 regex_07 = 'dgbhkfxj'
