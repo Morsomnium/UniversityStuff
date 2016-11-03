6c3a34533d647dbdb606ed8828b3fca488727c8a
Egils Looga <edloog@ttu.ee>
2016-11-03 13:35:57 +0200

    EX08 2.0

--- EX08/EX08.py
+++ EX08/EX08.py
@@ -1,6 +1,6 @@
 """Regex-play."""
 regex_01 = '^\w(at)\w?$'  # pass
-regex_02 = '(\w(a\wa)+)|((a\wa)+\w)'
+regex_02 = '^((\w(a\wa)+)|((a\wa)+\w))$'
 regex_03 = '^(((\w|\s)*(\S*\s{2}\S*)+(\w|\s)*)|())$'  # -
 regex_04 = '^0x[a-f0-9]+$'  # pass
 regex_05 = '^(1|0)*(1001)+(1|0)*$'  # pass
