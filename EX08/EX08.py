573522390e7b1374ad3e996bb3e5223fc649562f
Egils Looga <edloog@ttu.ee>
2016-11-03 12:16:21 +0200

    EX08 1.3

--- EX08/EX08.py
+++ EX08/EX08.py
@@ -3,6 +3,6 @@ regex_01 = '^\w(at)\w?$'  #pass+
 regex_02 = 'jbnffkjbn'
 regex_03 = '(  )*'  # -
 regex_04 = '^0x[a-f0-9]+$'  # pass
-regex_05 = '^(1001)+$'  # pass
+regex_05 = '^(1|0)*(1001)+(1|0)*$'  # pass
 regex_06 = '^(0[^0]*0[^0]*)*$'
 regex_07 = 'dgbhkfxj'
