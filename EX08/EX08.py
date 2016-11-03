094139a99f65e8e0a8174e1627f4894dd8bc6f89
Egils Looga <edloog@ttu.ee>
2016-11-03 12:09:36 +0200

    EX08 1.1

--- EX08/EX08.py
+++ EX08/EX08.py
@@ -1,8 +1,8 @@
 """Regex-play."""
-regex_01 = '^\w(at)\w?$'  #pass
+regex_01 = '^\w(at)\w?$'  #pass+
 regex_02 = 'jbnffkjbn'
-regex_03 = '\s*'  # pass
-regex_04 = '^0x[a-f0-9]+'  # pass
-regex_05 = '(1001)+'  # pass
+regex_03 = '(  )*'  # -
+regex_04 = '^0x[a-f0-9]+$'  # pass
+regex_05 = '^(1001)+$'  # pass
 regex_06 = '^(0[^0]*0[^0]*)*$'
 regex_07 = 'dgbhkfxj'
