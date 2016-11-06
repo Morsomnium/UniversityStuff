d582020b04de80ed4886467d22d90d847ddd2140
Egils Looga <edloog@ttu.ee>
2016-11-06 21:15:44 +0200

    EX09 0.0

---
+++ EX09/EX09.py
@@ -0,0 +1,20 @@
+"""Sentence generator."""
+
+
+class SentenceGenerator:
+
+    def __init__(self, rules_string):
+        rules1 = {}
+        rule_rows = rules_string.split('\n')
+        for row in rule_rows:
+            row = row.split()
+            rule = row[0]
+            row = ''.join(row[2:])
+            words = row.split('|')
+            rules1[rule] = words
+        self.rules = rules1
+
+    def sentence_generator(self, syntax):
+        while True:
+            for word in self.rules[syntax]:
+                yield word
