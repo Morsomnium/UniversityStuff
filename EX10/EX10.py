5b103977c17b7917b5d7a1758629dcc3b8149378
Egils Looga <edloog@ttu.ee>
2016-11-30 19:02:17 +0200

    EX10 2.0

--- EX10/EX10.py
+++ EX10/EX10.py
@@ -2,6 +2,7 @@
 from PIL import Image
 
 
+
 class Fractal:
     """Fractal."""
 
@@ -18,14 +19,18 @@ class Fractal:
         self.scale = scale
         self.computation = computation
         self.img = Image.new("RGB", self.size, 0)
-        self.pixel_dict = {}
+        self.img_list = []
 
     def compute(self):
         """Create the fractal by computing every pixel value."""
         for y in range(self.size[1]):
             for x in range(self.size[0]):
+                print("Computing", x, ":", y, "...")
                 i = self.pixel_value((x, y))
-                self.img.putpixel((x, y), (i % 4 * 64, i % 8 * 32, i % 16 * 16))
+                r = i % 4 * 64
+                g = i % 8 * 32
+                b = i % 16 * 16
+                self.img.putpixel((x, y), b * 65536 + g * 256 + r)
 
     def pixel_value(self, pixel):
         """
@@ -37,7 +42,19 @@ class Fractal:
         Returns:
         the number of iterations of computation it took to go out of bounds as integer.
         """
-        return self.computation(pixel)
+        if __name__ == "__main__":
+            xa = self.scale[0][0]
+            ya = self.scale[0][1]
+            xb = self.scale[1][0]
+            yb = self.scale[1][1]
+            imgx = self.size[0]
+            imgy = self.size[1]
+            maxit = 20
+            zx = pixel[0] * (xb - xa) / (imgx - 1) + xa
+            zy = pixel[1] * (yb - ya) / (imgy - 1) + ya
+            return self.computation((zx, zy, maxit))
+        else:
+            return self.computation(pixel)
 
     def save_image(self, filename):
         """
@@ -46,12 +63,47 @@ class Fractal:
         Arguments:
         filename -- the file name to save the file to as a string.
         """
-        self.img.save(filename, 'PNG')
+        self.img.save(filename)
+
+    def gif_create(self, frames):
+        img_list = []
+        xa = self.scale[0][0]
+        ya = self.scale[0][1]
+        xb = self.scale[1][0]
+        yb = self.scale[1][1]
+        b = 1
+        for i in range(1, frames+1):
+            temp_img = Fractal((256, 256), [(xa, ya), (xb, yb)], mandelbrot_computation)
+            temp_img.compute()
+            img_list.append(temp_img)
+            temp_img.save_image("mandelbrot" + str(i) + ".png")
+            xa *= 0.9
+            xb *= 0.9
+            ya *= 0.9
+            yb *= 0.9
+        print(img_list)
+        self.img_list = img_list
+
+    def gif_save(self, filename):
+        print(self.img_list)
+        self.img.save(filename, save_all=True, append_images=[self.img_list])
+
+    def image_show(self):
+        """Show."""
+        self.img.show()
 
 if __name__ == "__main__":
     def mandelbrot_computation(pixel):
         """Try."""
-        return pixel[0] + pixel[1]
-    mandelbrot = Fractal((1000, 1000), [(-2, -2), (2, 2)], mandelbrot_computation)
+        z = complex(0, 0)
+        c = complex(pixel[0], pixel[1])
+        for i in range(pixel[2]):
+            if z.real ** 2 + z.imag ** 2 >= 4:
+                break
+            z = z * z + c
+        return i
+    mandelbrot = Fractal((256, 256), [(-2, -2), (2.8, 2)], mandelbrot_computation)
     mandelbrot.compute()
-    #mandelbrot.save_image("mandelbrot.png")
+    #mandelbrot.image_show()
+    mandelbrot.gif_create(3)
+    mandelbrot.gif_save("mandelbrot.gif")
